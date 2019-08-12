106cd2f149d7d0887143d91193c24122c39476d7
xdghcnt <xd-mail@ya.ru>
2019-08-12 19:54:59 +0300

    Resources added

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 const
     Discord = require("discord.js"),
-    fs = require("fs"),
     gm = require("gm"),
+    fs = require("fs"),
+    randomFile = require('select-random-file'),
     express = require('express'),
     socketIo = require("socket.io");
 
 const
-    discordClients = {},
-    sessions = {};
+    rooms = {},
+    states = {},
+    playSettings = {
+        seek: 0,
+        volume: 0.3,
+        bitrate: 96,
+        passes: 1
+    },
+    saveSettings = () => {
+        const settings = {};
+        Object.keys(rooms).forEach((id) => {
+            settings[id] = {
+                token: rooms[id].token,
+                server: rooms[id].server,
+                voice: rooms[id].voice,
+                text: rooms[id].text,
+                timerLength: rooms[id].timerLength
+            };
+        });
+        fs.writeFile(`${__dirname}/settings.json`, JSON.stringify(settings), () => {
+        });
+    },
+    settings = JSON.parse(fs.readFileSync(`${__dirname}/settings.json`, "utf8"));
 
 // Server part
 const app = express();
@@ -21,75 +43,248 @@ app.get('/chgk', function (req, res) {
 const server = app.listen(1499);
 
 const io = socketIo(server);
-
 io.on("connection", socket => {
-    let discordClient, state, textChannel, voiceChannel, voiceConnection, user, voiceDispatcher;
-    const update = () => socket.emit("state", state);
+    let room, state;
+    const
+        update = () => io.to(room.roomId).emit("state", room),
+        playSoundConnection = (file, loop) => {
+            room.playing = false;
+            state.voiceDispatcher = state.voiceConnection.playFile(`${__dirname}/resources/sounds/${file}`, playSettings);
+            room.playingFile = file;
+            room.playing = true;
+            room.stopping = false;
+            update();
+            state.voiceDispatcher.on("end", () => {
+                if (loop && room.playing)
+                    state.voiceDispatcher = state.voiceConnection.playFile(`${__dirname}/resources/sounds/${file}`, playSettings);
+                else {
+                    room.playing = false;
+                    state.voiceChannel.leave();
+                    state.voiceConnection = null;
+                    update();
+                }
+            });
+        },
+        playSound = (file, loop) => {
+            if (state.voiceDispatcher)
+                state.voiceDispatcher.destroy();
+            if (state.voiceConnection)
+                playSoundConnection(file, loop);
+            else
+                state.voiceChannel.join().then((connection) => {
+                    state.voiceConnection = connection;
+                    playSoundConnection(file, loop);
+                })
+        },
+        stopSound = () => {
+            room.playing = false;
+            update();
+            if (state.voiceConnection && state.voiceDispatcher) {
+                room.stopping = true;
+                update();
+                const
+                    startVolume = state.voiceDispatcher.volume,
+                    interval = setInterval(() => {
+                        if (room.stopping) {
+                            state.voiceDispatcher.setVolume(state.voiceDispatcher.volume - startVolume / 20);
+                            if (state.voiceDispatcher.volume < 0) {
+                                clearInterval(interval);
+                                room.playing = false;
+                                room.stopping = false;
+                                state.voiceDispatcher.end();
+                                update();
+                            }
+                        } else clearInterval(interval);
+                    }, 100)
+            }
+        },
+        sendMessage = (message, opts) => {
+            if (state.textChannel)
+                state.textChannel.send(message, opts);
+        },
+        sendImage = (image) => sendMessage("", {files: [`${__dirname}/resources/images/${image}`]}),
+        sendScore = () => {
+            gm(280, 230, "rgb(134, 185, 254)")
+                .fill("rgb(48, 65, 171)")
+                .drawRectangle(140, 0, 280, 230)
+                .font(`${__dirname}/resources/Arial Bold.ttf`, 188)
+                .drawText(15, 180, room.expertsScore)
+                .fill("rgb(134, 185, 254)")
+                .drawText(160, 180, room.viewersScore)
+                .write(`${__dirname}/resources/images/score.png`, () => sendImage("score.png"));
+        },
+        gameEnd = () => {
+            playSound("ending.mp3");
+            if (room.expertsScore === 6)
+                randomFile(`${__dirname}/resources/images/win`, (err, file) => sendImage(`win/${file}`));
+            else
+                randomFile(`${__dirname}/resources/images/lose`, (err, file) => sendImage(`lose/${file}`));
+        },
+        startTimer = (isShort) => {
+            clearInterval(state.timer);
+            room.timer = (isShort ? 20 : room.timerLength) * 1000;
+            playSound("signal-main.ogg");
+            let time = new Date();
+            state.timer = setInterval(() => {
+                room.timer -= new Date() - time;
+                time = new Date();
+                if (!isShort && room.timer <= (11 * 1000) && room.timer > (10 * 1000))
+                    playSound("signal-warn.ogg");
+                else if (room.timer <= 0) {
+                    endTimer(isShort);
+                    update();
+                }
+            }, 1000);
+        },
+        endTimer = (isShort) => {
+            room.timer = null;
+            clearInterval(state.timer);
+            playSound(!isShort ? "signal-main.ogg" : "signal-blitz.ogg")
+        };
     socket.use((packet, next) => {
-        if (packet[0] === "init" || state)
+        if (packet[0] === "init" || room)
             return next();
         else
             socket.disconnect(true);
     });
-    socket.on("init", (userId) => {
-        user = userId;
-        sessions[userId] = sessions[userId] || {};
-        state = sessions[userId];
-        if (!state.inited) {
-            discordClient = discordClients[userId] = new Discord.Client();
-            state.inited = true;
+    socket.on("init", (initArgs) => {
+        states[initArgs.roomId] = states[initArgs.roomId] || {};
+        rooms[initArgs.roomId] = rooms[initArgs.roomId] || Object.assign({
+            inited: false,
+            roomId: initArgs.roomId,
+            expertsScore: 0,
+            viewersScore: 0,
+            timerLength: 60
+        }, settings[initArgs.roomId] || {});
+        room = rooms[initArgs.roomId];
+        state = states[initArgs.roomId];
+        if (!room.inited) {
+            state.discordClient = new Discord.Client();
+            room.inited = true;
         }
+        socket.join(initArgs.roomId);
         update();
     });
     socket.on("bot-token", (token) => {
-        state.prevToken = token;
-        if (discordClient)
-            discordClient.login(token).then(() => {
-                state.discordClientReady = true;
+        room.token = token;
+        if (state.discordClient)
+            state.discordClient.login(token).then(() => {
+                room.discordClientReady = true;
+                saveSettings();
                 update();
             }).catch((error) => socket.emit("message", error.message));
     });
     socket.on("logout-bot", () => {
-        state.discordClientReady = false;
-        state.voiceConnected = false;
-        if (discordClient)
-            discordClient.destroy();
-        discordClient = discordClients[user] = new Discord.Client();
+        room.discordClientReady = false;
+        room.voiceConnected = false;
+        if (state.discordClient)
+            state.discordClient.destroy();
+        state.discordClient = new Discord.Client();
         update();
     });
     socket.on("send-message", (message) => {
-        if (textChannel)
-            textChannel.send(message);
+        sendMessage(message);
     });
-    socket.on("play-sound", () => {
-        if (voiceConnection)
-            voiceDispatcher = voiceConnection.playFile(`./middle2.mp3`, {seek: 0, volume: 0.3, passes: 1});
+    socket.on("play-sequence", (name) => {
+        if (name === "intro") {
+            sendImage("logo.png");
+            playSound("intro.mp3");
+        } else if (name === "ambient") {
+            playSound("ambient.mp3");
+        } else if (name === "horse") {
+            playSound("horse.mp3");
+            sendImage("volchok.jpg");
+        } else if (name === "black-box") {
+            playSound("box.mp3");
+            sendImage("blackbox.jpg");
+        } else if (name === "sych") {
+            playSound("sych.mp3");
+        } else if (name === "pause") {
+            playSound("pause.mp3", true);
+            sendImage("music.png");
+        } else if (name === "13-sector") {
+            playSound("13sector.mp3");
+            sendImage("13sector.jpg");
+        } else if (name === "win-round") {
+            room.expertsScore++;
+            if (room.expertsScore === 6)
+                gameEnd();
+            else {
+                randomFile(`${__dirname}/resources/sounds/win-round`, (err, file) => playSound(`win-round/${file}`, true));
+                randomFile(`${__dirname}/resources/images/win`, (err, file) => sendImage(`win/${file}`));
+            }
+            update();
+            sendScore();
+        } else if (name === "lose-round") {
+            room.viewersScore++;
+            if (room.viewersScore === 6)
+                gameEnd();
+            else {
+                randomFile(`${__dirname}/resources/sounds/lose-round`, (err, file) => playSound(`lose-round/${file}`, true));
+                randomFile(`${__dirname}/resources/images/lose`, (err, file) => sendImage(`lose/${file}`));
+            }
+            update();
+            sendScore();
+        } else if (name === "gong") {
+            if (room.viewersScore === 0 && room.expertsScore === 0)
+                playSound("gong-start.mp3");
+            else if (room.viewersScore === 6 || room.expertsScore === 6)
+                playSound("gong-end.mp3");
+            else
+                randomFile(`${__dirname}/resources/sounds/gong`, (err, file) => playSound(`gong/${file}`));
+            update();
+        }
+    });
+    socket.on("start-timer", () => {
+        startTimer();
+        update();
+    });
+    socket.on("start-timer-short", () => {
+        startTimer(true);
+        update();
+    });
+    socket.on("reset-timer", () => {
+        clearInterval(state.timer);
+        room.timer = null;
+        update();
+    });
+    socket.on("set-timer", (timerLength) => {
+        if (!isNaN(timerLength))
+            room.timerLength = timerLength;
+        saveSettings();
+        update();
     });
     socket.on("stop-sound", () => {
-        if (voiceConnection && voiceDispatcher)
-            voiceDispatcher.end();
+        stopSound();
+    });
+    socket.on("set-score", (experts, viewers) => {
+        room.expertsScore = parseInt(experts) || 0;
+        room.viewersScore = parseInt(viewers) || 0;
+        sendScore();
+        update();
     });
     socket.on("discord-params", (params) => {
-        state.prevServer = params.server;
-        state.prevText = params.text;
-        state.prevVoice = params.voice;
-        if (state.discordClientReady && discordClient) {
-            const guild = discordClient.guilds && discordClient.guilds.find("name", params.server);
+        room.server = params.server;
+        room.text = params.text;
+        room.voice = params.voice;
+        if (room.discordClientReady && state.discordClient) {
+            const guild = state.discordClient.guilds && state.discordClient.guilds.find("name", params.server);
             if (guild) {
-                textChannel = guild.channels.find(channel => channel.name === params.text && channel.type === "text");
-                voiceChannel = guild.channels.find(channel => channel.name === params.voice && channel.type === "voice");
-                if (!textChannel || !voiceChannel)
+                state.textChannel = guild.channels.find(channel => channel.name === params.text && channel.type === "text");
+                state.voiceChannel = guild.channels.find(channel => channel.name === params.voice && channel.type === "voice");
+                if (!state.textChannel || !state.voiceChannel)
                     socket.emit("message", "Text or voice channel not found");
                 else
-                    voiceChannel.join().then((connection) => {
-                        voiceConnection = connection;
-                        state.voiceConnected = true;
+                    state.voiceChannel.join().then(() => {
+                        saveSettings();
+                        state.voiceChannel.leave();
+                        room.voiceConnected = true;
                         update();
                     }).catch((error) => socket.emit("message", error.message))
             } else socket.emit("message", "Server not found");
         } else {
-            state.discordClientReady = false;
-            discordClient = discordClients[user] = new Discord.Client();
+            room.discordClientReady = false;
+            state.discordClient = new Discord.Client();
             update();
         }
     })
--- public/app.jsx
+++ public/app.jsx
@@ -12,13 +12,21 @@ function makeId() {
 
 class App extends React.Component {
     componentDidMount() {
+        const initArgs = {};
         localStorage.userId = localStorage.userId || makeId();
+        if (!location.hash)
+            location.hash = makeId();
+        initArgs.roomId = location.hash.substr(1);
+        initArgs.userId = localStorage.userId;
         this.socket = io();
         this.socket.on("state", state => {
             this.setState(Object.assign({}, state));
         });
         this.socket.on("message", message => alert(message));
-        this.socket.emit("init", localStorage.userId);
+        this.socket.on("disconnect", () => {
+            this.setState(Object.assign({}, {inited: false}));
+        });
+        this.socket.emit("init", initArgs);
     }
 
     constructor() {
@@ -49,8 +57,28 @@ class App extends React.Component {
         document.getElementById("message").value = "";
     }
 
-    playSound() {
-        this.socket.emit("play-sound");
+    playSequence(name) {
+        this.socket.emit("play-sequence", name);
+    }
+
+    setScore() {
+        this.socket.emit("set-score", prompt("Experts"), prompt("Viewers"));
+    }
+
+    startTimer() {
+        this.socket.emit("start-timer");
+    }
+
+    startTimerShort() {
+        this.socket.emit("start-timer-short");
+    }
+
+    resetTimer() {
+        this.socket.emit("reset-timer");
+    }
+
+    setTimer() {
+        this.socket.emit("set-timer", prompt("Timer seconds"));
     }
 
     stopSound() {
@@ -58,43 +86,124 @@ class App extends React.Component {
     }
 
     render() {
+        clearTimeout(this.timeOut);
         if (this.state.inited) {
+            const timerSecondDiff = ((this.state.timer % 100) || 100);
+            if (this.state.timer - timerSecondDiff > 0) {
+                let timeStart = new Date();
+                this.timeOut = setTimeout(() => {
+                    if (this.state.timer)
+                        this.setState(Object.assign({}, this.state, {timer: this.state.timer - (new Date() - timeStart)}));
+                }, timerSecondDiff);
+            }
             return (<div>
                 <div className="token" style={{display: this.state.discordClientReady ? "none" : "block"}}>
-                    Bot token: <input type="text" id="token" defaultValue={this.state.prevToken}/>
+                    Bot token: <input type="text" id="token" defaultValue={this.state.token}/>
                     <i className="material-icons button" onClick={() => this.submitBotToken()}>
                         send
                     </i>
                 </div>
-                <div className="params" style={{display: !this.state.discordClientReady || this.state.voiceConnected ? "none" : "block"}}>
-                    Server name: <input type="text" id="server" defaultValue={this.state.prevServer}/><br/>
-                    Voice channel: <input type="text" id="voice" defaultValue={this.state.prevVoice}/><br/>
-                    Text channel: <input type="text" id="text" defaultValue={this.state.prevText}/>
+                <div className="params"
+                     style={{display: !this.state.discordClientReady || this.state.voiceConnected ? "none" : "block"}}>
+                    <span>Server name:</span><input type="text" id="server" defaultValue={this.state.server}/><br/>
+                    <span>Voice channel:</span><input type="text" id="voice" defaultValue={this.state.voice}/><br/>
+                    <span>Text channel:</span><input type="text" id="text" defaultValue={this.state.text}/>
                     <i className="material-icons button" onClick={() => this.submitDiscordParams()}>
                         send
                     </i>
                 </div>
                 <div className="features" style={{display: !this.state.voiceConnected ? "none" : "block"}}>
                     Send message: <input type="text" id="message"/>
-                    <i className="material-icons button" onClick={() => this.sendMessage()}>
+                    <div className="material-icons button" onClick={() => this.sendMessage()}>
                         send
-                    </i>
-                    <br/>
-                    <i className="material-icons button" onClick={() => this.playSound()}>
-                        play_arrow
-                    </i>
-                    <i className="material-icons button" onClick={() => this.stopSound()}>
-                        stop
-                    </i>
+                    </div>
+                    <hr/>
+                    <div className="sounds">
+                        <div className="button" onClick={() => this.playSequence("intro")}>
+                            Intro
+                        </div>
+                        <div className="button" onClick={() => this.playSequence("horse")}>
+                            Horse
+                        </div>
+                        <div className="button" onClick={() => this.playSequence("black-box")}>
+                            Black box
+                        </div>
+                        <div className="button" onClick={() => this.playSequence("13-sector")}>
+                            13 sector
+                        </div>
+                        <div className="button" onClick={() => this.playSequence("pause")}>
+                            Pause
+                        </div>
+                        <div className="button" onClick={() => this.playSequence("sych")}>
+                            Sych
+                        </div>
+                    </div>
+                    <div className="score">
+                        <div className="score-number experts">{this.state.expertsScore}</div>
+                        <div className="score-number viewers">{this.state.viewersScore}</div>
+                        <div className="score-buttons">
+                            <i className="material-icons button score-button"
+                               onClick={() => this.playSequence("win-round")}>
+                                add_box
+                            </i>
+                            <i className="material-icons button score-button"
+                               onClick={() => this.playSequence("lose-round")}>
+                                add_box
+                            </i>
+                        </div>
+                    </div>
+                    <div className="sounds-right">
+                        <div className="button" onClick={() => this.playSequence("gong")}>
+                            Gong
+                        </div>
+                        <div className="button" onClick={() => this.startTimer()}>
+                            Start timer
+                        </div>
+                        <div className="button" onClick={() => this.startTimerShort()}>
+                            Start short timer
+                        </div>
+                        <div className="button" onClick={() => this.setTimer()}>
+                            Set timer
+                        </div>
+                        <div className="button" onClick={() => this.setScore()}>
+                            Set score
+                        </div>
+                    </div>
+                    {this.state.timer ? (<div className="timer">
+                        {this.state.timer && (new Date(this.state.timer)).toUTCString().match(/(\d\d:\d\d )/)[0].trim()}
+                        <div className="material-icons button timer-reset" onClick={() => this.resetTimer()}>
+                            close
+                        </div>
+                    </div>) : ""}
+                    <hr/>
+                    <div>
+                        Playing: <span>{
+                        this.state.stopping ? "fading" : (this.state.playing ? this.state.playingFile : "nothing")
+                    }</span>
+                    </div>
+                    {this.state.playing ? (<div className="button" onClick={() => this.stopSound()}>
+                        <i className="material-icons">
+                            stop
+                        </i>Stop
+                    </div>) : ""}
+                    <hr/>
                 </div>
-                <div className="logout button" style={{display: !this.state.discordClientReady ? "none" : "inline-block"}} onClick={() => this.logoutFromBot()}>
-                    <i className="material-icons">
-                        exit_to_app
-                    </i>Logout
+                <div className="footer">
+                    <div className="logout button"
+                         style={{display: !this.state.discordClientReady ? "none" : "inline-block"}}
+                         onClick={() => this.logoutFromBot()}>
+                        <i className="material-icons">
+                            exit_to_app
+                        </i>Logout
+                    </div>
+                    <div className="status" style={{display: !this.state.voiceConnected ? "none" : "block"}}>
+                        {this.state.server} / {this.state.text} / {this.state.voice}
+                    </div>
                 </div>
+
             </div>);
         }
-        else return (<div/>);
+        else return (<div>Disconnected</div>);
     }
 }
 
